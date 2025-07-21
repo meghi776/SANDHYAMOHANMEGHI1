dceee9a709f31a2a34246cf63d5c225f01f5afb7
meghi776 <puppalameghi@gmail.com>
2025-07-21 12:12:58 +0530

    Reverted all changes back to version a1ebd529ad6217f83d072d79446b19c47967dfd8

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -88,7 +88,7 @@ const Login = () => {
             </div>
             <Alert>
               <Info className="h-4 w-4" />
-              <AlertTitle>Already Placed an Order?</AlertTitle>
+              <AlertTitle>First Time User?</AlertTitle>
               <AlertDescription>
                 If you checked out as a guest, your password is your 10-digit phone number. Please change it in 'My Account' after logging in.
               </AlertDescription>
